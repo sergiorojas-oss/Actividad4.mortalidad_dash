6ac942a9ef878b680df62b8301e912bcefdd900f
sergiorojas-oss <sergio.aro@hotmail.com>
2026-05-15 13:42:30 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -179,5 +179,8 @@ app.layout = html.Div(
     ]
 )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8050)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port)
